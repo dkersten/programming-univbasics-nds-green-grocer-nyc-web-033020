650f603af5688f36c79a654a4314a49e7b54f11b
Daniel Kersten <dkersten1994@gmail.com>
2020-03-15 18:57:15 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -81,7 +81,7 @@ def apply_clearance(cart)
   
   while i < cart.length do
     if cart[i][:clearance]
-      cart[i][:price] *= .8
+      cart[i][:price] *= 0.8
       cart[i][:price].round(2)
     end
     
